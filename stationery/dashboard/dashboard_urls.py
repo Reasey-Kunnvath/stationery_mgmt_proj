6d655e472179db119fe9f4392d705b6b7d86afdc
Panda <daphadenphorakden@gmail.com>
2025-08-01 16:31:26 +0700

    Make Category Done

--- stationery/dashboard/dashboard_urls.py
+++ stationery/dashboard/dashboard_urls.py
@@ -4,6 +4,18 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard_home, name='dashboard_home'),
+    
+    #Category Routes
     path('category', views.category, name='category'),
+    path('category/create/', views.categoryCreate, name='category_create'),
+    path('category/store/', views.categoryStore, name='category_store'),
+    path('category/edit/<int:id>/', views.categoryEdit, name='category_edit'),
+    path('category/update/<int:id>/', views.categoryUpdate, name='category_update'),
+    path('category/delete/<int:id>/', views.categoryDelete, name='category_delete'),
+
+
+    #Item Routes
     path('users', views.user, name='user'),
+    path('item', views.item, name='item'),
+    
 ]
