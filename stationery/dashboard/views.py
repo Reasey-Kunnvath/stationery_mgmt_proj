e870d99f1883606bf31d1406791f14d965e7a702
Panda <daphadenphorakden@gmail.com>
2025-08-07 16:29:31 +0700

    Dashboard

--- stationery/dashboard/views.py
+++ stationery/dashboard/views.py
@@ -1,5 +1,20 @@
+from itertools import count
 from django.shortcuts import render ,redirect
 from .models import *
+from user.models import *
+from login.models import *
+from dashboard.models import *
+from django.db.models import Avg, Count, Min, Sum
 import sweetify # type: ignore
 def dashboard_home(request):
-    return render(request, 'index.html')
+    order_count = Order.objects.count()
+    user_count = Users.objects.count()
+    stock_out_count = StockOut.objects.values('order_id').distinct().count()
+    stock_in_count = StockIn.objects.aggregate(total_qty=Sum("quantity"))['total_qty']
+    context = {
+        'order_count': order_count,
+        'user_count': user_count,
+        'stock_out_count': stock_out_count,
+        'stock_in_count': stock_in_count
+    }
+    return render(request, 'index.html', context)
